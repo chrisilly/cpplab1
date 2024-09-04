5172cb94c43e34754de2e5a2843db1c8fe51ebed
Chris <103900975+culliiing@users.noreply.github.com>
2024-09-04 13:23:27 +0200

    Adds input that print to console

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
-int main()
+int main(int argc, const char* argv[])
 {
-	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello World! ";
+
+	for (int i = 0; i < argc; i++)
+	{
+		std::cout << argv[i];
+		std::cout << " ";
+	}
 
 	return 0;
 }
