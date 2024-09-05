812ada6b1736720ec3886fa982b83856342c6e51
Chris <christian@culling.se>
2024-09-05 10:40:41 +0200

    basic test case

--- poly/polysolver.cpp
+++ poly/polysolver.cpp
@@ -5,9 +5,15 @@ int main(int argc, const char* argv[])
 {
 	std::cout << "Root-finding started..." << std::endl;
 
-    
-    // todo
+	// Make dynamic way to get coefficients. Pull from file?
+    float a=1, b=2, c=1;
+	float x=-1;
 
+	// Logic & testing
+	Poly2 polysolver(a, b, c);
+	
+	polysolver.findRoots();
+	std::cout << "The value of y for x = " << x << ": " << polysolver.eval(x);
     
 	return 0;
 }
