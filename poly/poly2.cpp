4be12639d2c2d37470abeaa3634c8771fc57f7e3
Chris <christian@culling.se>
2024-09-05 10:34:41 +0200

    implements polynomial calculations

--- poly/poly2.cpp
+++ poly/poly2.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
 #include "poly2.h"
 #include <cmath>
+
+using namespace std;
 
 Poly2::Poly2(float a, float b, float c)
 {
@@ -10,14 +13,32 @@ Poly2::Poly2(float a, float b, float c)
 
 float Poly2::eval(float x)
 {
-    
-    // todo
-
+	return (a * x * x) + (b * x) + c;
 }
 
 void Poly2::findRoots()
 {
+    float root1;
+	float root2;
     
-    // todo
+	float d = sqrt((b*b) - 4 * a * c);
 
+	if(d > 0)
+	{
+		root1 = (d-b)/(2*a);
+		root2 = (-d-b)/(2*a);
+
+		std::cout << "Two roots were found: ";
+		std::cout << root1 << ", " << root2 << endl;
+	}
+	else if(d == 0)
+	{
+		root1 = (-b)/(2*a);
+		std::cout << "One root was found: ";
+		std::cout << root1 << endl;
+	}
+	else
+	{
+		std::cout << "Unable to find any real roots." << endl;
+	}
 }
